a8c3b6ceef1da809b824fea305871e749af7b7a9
hyuntony <ankim90@gmail.com>
2021-06-04 01:32:07 -0400

    added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request');
-const myArgs = process.argv.slice(2);
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${myArgs[0]}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Error: Requested breed is not found!");
-    return;
-  }
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("Error: Requested breed is not found!", null);
+      return;
+    }
+    callback(null, data[0].description);
+  });
+
+};
+
+module.exports = fetchBreedDescription;
+
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,26 @@
+const fetchBreedDescription = require('../breedFetcher');
+const {assert} = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (hello) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+      assert.equal(expectedDesc, desc.trim());
+
+      hello();
+    });
+  });
+  it('returns a err for an invalid breed, and desc to be null', (done) => {
+    fetchBreedDescription('hellothere', (err, desc) => {
+      assert.equal(desc, null);
+
+      const expectedError = "Error: Requested breed is not found!";
+      assert.equal(err.trim(), expectedError);
+
+      done();
+    });
+  });
+});
+
